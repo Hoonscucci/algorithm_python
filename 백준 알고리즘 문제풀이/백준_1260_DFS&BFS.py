02104928a2bf8806c9c0201c9f091da520f8dbb5
이상훈 <132039559+Hoonscucci@users.noreply.github.com>
2023-07-13 17:43:34 +0900

    Add files via upload

---
+++ 백준 알고리즘 문제풀이/백준_1260_DFS&BFS.py
@@ -0,0 +1,74 @@
+문제
+그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
+
+입력
+첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
+
+출력
+첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
+
+예제 입력 1 
+4 5 1
+1 2
+1 3
+1 4
+2 4
+3 4
+예제 출력 1 
+1 2 4 3
+1 2 3 4
+
+===================================
+
+"""
+깊이우선과 너비우선에 대하여 미리 코드 구현과 이론을 학습했지만
+문제를 맞추기에는 내가 온전하게 학습하지 못했던것 같다.
+이해 했다고 생각했던 부분들이 막상 문제로 나오니 
+"""
+
+from collections import deque
+
+
+def dfs(start):
+    visited[start] = True
+    print(start, end=" ")
+
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(i)
+
+
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 정렬
+for i in graph:
+    i.sort()
+
+# dfs
+visited = [False] * (N + 1)
+dfs(V)
+print()
+
+# bfs
+visited = [False] * (N + 1)
+bfs(V)
+   
